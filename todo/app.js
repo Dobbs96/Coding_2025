3002c195f259ec92963dfc6e80bea49b3c772f79
James Dobbs <dobbs282@gmail.com>
2025-02-14 05:56:12 -0500

    add event working

--- todo/app.js
+++ todo/app.js
@@ -1,1 +1,11 @@
-//works
+// Global varible
+
+const todoForm = document.querySelector("form");
+const todoInput = document.getElementById("todo-input");
+const todoListUL = document.getElementById("todo-list");
+
+let allTodos = [];
+
+todoForm.addEventListener("submit", (e) => {
+  alert("add");
+});
